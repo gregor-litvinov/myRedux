33b0186490423fa8c0375e120084d498ca4ed92f
System Administrator <root@MacBook-Pro-Alex.local>
2022-08-07 13:55:37 +0300

    myRedux

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 import './styles.css'
+import { createStore } from './createStore';
+import { rootReducer } from './redux/rootReducer';
+
 const counter = document.getElementById("counter");
 const addBtn = document.getElementById("add")
 const subBtn = document.getElementById("sub")
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 0
-
-function render() {
-  counter.textContent = state.toString()
-}
+const store = createStore(rootReducer, 0)
 
 addBtn.addEventListener("click", () => {
-  state++;
-  render();
+store.dispatch({type: 'INCREMENT'})
 }),
   subBtn.addEventListener("click", () => {
-    state--;
-    render();
+store.dispatch({ type: "DECREMENT" });
   }),
   asyncBtn.addEventListener("click", () => {
-   setTimeout(() => {
-     state++
-     render()
-   }, 3000)
+
   }),
+
+store.subscribe(() => {
+  const state = store.getState()
+
+  counter.textContent = state
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
+
     themeBtn.addEventListener("click", () => {
    document.body.classList.toggle('dark')
   })
-  render()
